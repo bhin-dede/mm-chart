235fa020b34f37954efd6aa1932a1f5e34ba76ef
bhin-dede <68467902+bhin-dede@users.noreply.github.com>
2024-11-01 16:08:14 +0900

    Add files via upload

---
+++ mm-chart.js
@@ -0,0 +1,37 @@
+Module.register("mm-chart", {
+    defaults: {
+        width       : 200,
+        height      : 200,
+        chartConfig : {}
+    },
+
+    getScripts () {
+        return [`modules/${this.name}/node_modules/chart.js/dist/chart.umd.js`];
+    },
+
+    start () {
+        Log.info(`Starting module: ${this.name}`);
+    },
+
+    getDom () {
+    // Create wrapper element
+        const wrapperEl = document.createElement("div");
+        wrapperEl.setAttribute("style", "position: relative; display: inline-block;");
+
+        // Create chart canvas
+        const chartEl = document.createElement("canvas");
+
+        // Init chart.js
+        this.chart = new Chart(chartEl.getContext("2d"), this.config.chartConfig);
+
+        // Set the size
+        chartEl.width  = this.config.width;
+        chartEl.height = this.config.height;
+        chartEl.setAttribute("style", "display: block;");
+
+        // Append chart
+        wrapperEl.appendChild(chartEl);
+
+        return wrapperEl;
+    }
+});
